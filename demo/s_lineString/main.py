21ada005530c434bda547f8dc96ca1336605134d
geoyee <geoyee@yeah.net>
2021-10-27 13:26:19 +0800

    add linestring

---
+++ demo/s_lineString/main.py
@@ -0,0 +1,14 @@
+## console
+import math
+import random
+from qgis.core import *
+from view import showgeoms
+
+
+lines = []
+pts = [QgsPoint(x, math.cos(x) + (x % 3 - 1) * random.random() * 5) \
+       for x in range(100)]
+L1 = QgsLineString()
+L1.setPoints(pts)
+showgeoms([L1], "L1")
+L1.addVertex(QgsPoint(100, 9))
---
+++ demo/s_lineString/view.py
@@ -0,0 +1,25 @@
+from qgis.core import *
+
+
+def getgtypes():
+    return "point", "linestring", "polygon", \
+           "multipoint", "multilinestring", \
+           "multipolygon"
+
+
+def showgeoms(geoms, name="tmp", gtype=None):
+    if gtype is None:
+        gtype = geoms[0].geometryType().lower()
+    if gtype not in getgtypes():
+        raise Exception("gtype should be one of :{" + \
+                        ','.join(getgtypes()) + "}")
+    vl = QgsVectorLayer(gtype, name, "memory")
+    pr = vl.dataProvider()
+    feats = []
+    for geom in geoms:
+        feat = QgsFeature()
+        feat.setGeometry(geom)
+        feats.append(feat)
+    pr.addFeatures(feats)
+    QgsProject.instance().addMapLayer(vl)
+    iface.zoomFull()
